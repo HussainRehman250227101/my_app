22b0389050f94c401e5527c7cea9a286be774413
hussain <hussain@gmail.com>
2025-11-02 01:58:41 +0500

    bugs and bugs and bugs

--- my_app/settings.py
+++ my_app/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY") 
+SECRET_KEY = 'Vb8Nw2RqTz6LpYf3Hm4KdXj1Uc7Gs9Ae5Sh0JrQvLZpWxM' 
 if not SECRET_KEY:
     raise ValueError("No SECRET_KEY set in environment!")
 
