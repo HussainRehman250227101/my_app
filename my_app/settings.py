c9347b98fe2d1b9db91b28cae38ac95d9b9e4ee9
hussain <hussain@gmail.com>
2025-11-02 01:53:12 +0500

    fuck bugs

--- my_app/settings.py
+++ my_app/settings.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 from pathlib import Path
 import os
+print("SECRET_KEY:", os.environ.get("SECRET_KEY"))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
